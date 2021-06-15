cf54602f264451f92a2dd2e800bfb5461903279f
marina <marinaaltesmagret@gmail.com>
2021-06-15 19:53:04 +0200

    Setting up project with bug in tailwindcss still,  testing beers api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      beers: ''
+    }
+  }
+
+  componentDidMount(){
+    axios.get("https://ih-beers-api2.herokuapp.com/beers").then(res => this.setState({beers: res.data}))
+  }
+  
+  render() {
+    const { beers } = this.state;
+    return (
+      <div className="App">
+        <ul>
+          {[...beers].map((beer, index) => <li key={index}>{beer.name}</li>)}
+          </ul>
+      </div>
+          );
+        }
+        
 }
 
 export default App;
